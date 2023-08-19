6d7da827ea408883d673ddfa19d5e451a139e6f2
gilbert <gilbert.bageni@student.moringaschool.com>
2023-08-19 12:57:20 +0300

    done converted 12hour hour to 24 hours

--- challenge1.py
+++ challenge1.py
@@ -1,6 +1,23 @@
-# define  a function to convert 12-hour time to 24-hour time
-def convert_to_24_hours(hour,time,period):
-    #check if the given period is "am"
-    if period == "AM":
+# Define a function to convert 12-hour time to 24-hour time
+def convert_to_24_hours(hour, minute, period):
+    # Check if the given period is "am"
+    if period == 'AM':
         # If it's midnight (12 am), set the hour to 0
+        if hour == 12:
+            hour = 0
+    else: 
+         # If it's not "am" (i.e., it's "pm")
+        # and the hour is not noon (12 pm), add 12 to the hour        
+        if hour != 12:
+            hour += 12
+            
+    # Use f-string formatting to format the hour and minute as two-digit strings
+    # The :02d inside the curly braces ensures that each number is at least two digits
+    # and is padded with leading zeros if needed
+    # Finally, concatenate the formatted hour and minute to create the 24-hour time
+    return f"{hour:02d}:{minute:02d}"
 
+# Test cases
+# Call the function with different inputs and print the results
+print(convert_to_24_hours(8, 30, "AM"))
+print(convert_to_24_hours(8, 30, "PM"))
