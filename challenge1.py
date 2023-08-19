4f81ac642b0950f58a5055acfa0a0cc5cf148963
gilbert <gilbert.bageni@student.moringaschool.com>
2023-08-19 12:50:49 +0300

    use the equal operator to convert time

--- challenge1.py
+++ challenge1.py
@@ -1,3 +1,6 @@
 # define  a function to convert 12-hour time to 24-hour time
 def convert_to_24_hours(hour,time,period):
-#check if the given period is "am"
+    #check if the given period is "am"
+    if period == "AM":
+        # If it's midnight (12 am), set the hour to 0
+
